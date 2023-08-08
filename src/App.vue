1cb3daf4a2fd8e35cbd2abeef531df9d7ca5ad54
InawoVictor <inawovictor004@gmail.com>
2023-08-08 14:18:26 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<!-- HTML -->
+<template>
+  <h2>{{title}}</h2>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal to="#modals" theme="sale" @close="toggleModal" >
+      <template v-slot:links>
+        <a href="#">Sign up</a>
+        <a href="#">Register</a>
+      </template>
+      <h1>{{header}}</h1>
+      <p>{{text}}</p>
+    </Modal>    
+  </div>
+  <teleport to="#modals" v-if="showModal2">
+    <Modal @close="toggleModal2">      
+      <h2>Thanks for your patronage</h2>
+      <p>Sing in to get the latest features</p>
+    </Modal>
+  </teleport>
+  <button @click.alt="toggleModal">Open Modal(alt)</button>
+  <button @click.alt="toggleModal2">Open Modal2(alt)</button>
+</template>
+
+<!-- SCRIPT -->
+<script>
+import Modal from "./components/Modal"
+
+export default {
+  name: 'App',
+  components:{
+    Modal
+  },
+  data() {
+    return {
+      title: "My first Vue App:)",
+      header: "Sign up for the give away",
+      text: "Grab your swag Fam, for half price",
+      showModal: false,
+      showModal2: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      return this.showModal = !this.showModal
+    },
+    toggleModal2() {
+      return this.showModal2 = !this.showModal2
+    }
+  }
+}
+</script>
+
+<!-- CSS -->
+<style>
+#app, #modals {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h2{
+  border-bottom: 1px solid rgb(190, 190, 190);
+  padding-bottom: 10px;
+  display: inline-block;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{sale: theme === 'sale' }"  >
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+//:class="{success : theme === 'success'}"
+export default {
+    props: ["theme"],
+    methods: {
+        closeModal() {
+            this.$emit("close")
+        }
+    }
+}
+
+</script>
+
+<style >
+    .backdrop{
+        position: fixed;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, .5);
+    }
+    .modal{
+        width: 400px;
+        padding: 20px;
+        border-radius: 10px;
+        background: #fff;
+        margin: 100px auto;
+    } 
+    .modal h2{
+        border: none;
+        padding: 0;
+        color: teal;
+    }
+    .modal .actions {
+        color: #333;
+        margin: 30px 10px 0;
+        text-align: center;
+    }
+    .modal .actions a {
+        color: #333;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        margin-right: 10px;
+        text-decoration: none;
+        padding: 8px;
+    }
+    .modal.sale{
+        background: crimson;
+        color: #fff;
+    }
+    .modal .sale h2 {
+        color: white;
+    }
+    .modal .sale .actions {
+        color: white;
+    }
+    .modal.sale .actions a {
+        color: white;
+    }
+    .modal.success{
+        background: teal;
+    }
+    .modal.sale .success a{
+        color: white;
+    }
+    .modal .success h2 {
+        color: white;
+    }
+</style>
